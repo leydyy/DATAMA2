a47a3b653a385defdec2afd67e567271308ab5d7
leydyy <lalainne10andaya@gmail.com>
2025-02-22 06:32:53 +0000

    .

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -3,10 +3,3 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-export const supabase = createApp(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true, // Ensures the token persists
-    autoRefreshToken: true,
-    detectSessionInUrl: true
-  }
-});
