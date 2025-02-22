ad8d94f536c48fdfaf120b0261fa3c24e13e5b87
leydyy <lalainne10andaya@gmail.com>
2025-02-22 07:35:12 +0000

    .

--- my-app/src/lib/supabaseClient.js
+++ my-app/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://shkxkunapgltgiouiazc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoa3hrdW5hcGdsdGdpb3VpYXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNjE4NzAsImV4cCI6MjA1NTYzNzg3MH0.TfoqE15HdzvCAju_0eIY-f44YUsCY1LtXTxtij_kpkw')
+export const supabase = createClient('https://rtrkbhtpjcqcshsatnxb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0cmtiaHRwamNxY3Noc2F0bnhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMDk0OTYsImV4cCI6MjA1NTc4NTQ5Nn0.5HwFvafPubYOSCIQxL1pdHQl40KOJsBK0c4Mgc0GtKw')
