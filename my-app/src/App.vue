edaa0edb76c4083e2dd82e8fefaa3de438c8e5f0
leydyy <lalainne10andaya@gmail.com>
2025-02-27 06:23:39 +0000

    .

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <Home/>
   <Clients/>
   <Motorcar/>
 </template>
 
 <script setup lang="ts">
+  import Home from './components/Home.vue';
   import Clients from './components/Clients.vue';
   import Motorcar from './components/Motorcar.vue';
 </script>
