ae4cad3bbd698ade9e203dda56d6559f8b76e2c9
leydyy <lalainne10andaya@gmail.com>
2025-02-28 09:06:56 +0000

    .

--- my-app/src/supabase.js
+++ my-app/src/supabase.js
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://shkxkunapgltgiouiazc.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNoa3hrdW5hcGdsdGdpb3VpYXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNjE4NzAsImV4cCI6MjA1NTYzNzg3MH0.TfoqE15HdzvCAju_0eIY-f44YUsCY1LtXTxtij_kpkw';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
